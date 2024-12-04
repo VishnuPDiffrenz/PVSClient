ad264f84a6fb5f26847a21013e02710f6d317627
VishnuP <vishnu@diffrenz.com>
2024-12-04 18:19:13 +0530

    Dashboard setup

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -5,36 +5,34 @@ import salesIcon from "../assets/salesIcon.svg";
 import crmIcon from "../assets/crmIcon.svg";
 import settingsIcon from "../assets/settingsIcon.svg";
 import bgImage from "../assets/pvsBg.jpg";
-
-{/* <div className="flex flex-col items-center justify-center mx-[20vw] my-[10vh] border border-slate-200 py-10 rounded-xl"> */}
+import crmImage from "../assets/crmImage.png";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-[100vh] my-16 sm:my-auto " style={{
+    <div className="flex flex-col items-center justify-center h-[100vh]" style={{
       backgroundImage: `url(${bgImage})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
     }}>
-      <div className="border border-slate-300 pb-6 px-10 rounded-lg bg-white mx-5">
-      <div className="text-center mb-2 pt-8">
+      <div className="border border-[#D6D6D6] pb-6 px-10 rounded-lg bg-[#FFFFFF] mx-5 max-w-[100vh]">
+      <div className="text-center mb-2 pt-12">
         <img
           src={pvsIcon}
           alt="PVS Logo"
           className="h-10 mx-auto"
         />
-        <h1 className="text-xl font-semibold text-gray-700 mt-4">Welcome back, Mauro.</h1>
+        <h1 className="text-md font-semibold text-[#5F5F5F] mt-8">Welcome back, Mauro.</h1>
       </div>
 
       <div className="flex flex-col items-center">
-        {/* User Dropdown */}
-        <div className="bg-[#EAFAFF] shadow-lg rounded-lg p-4 mb-6 flex items-center space-x-4">
+        <div className="bg-[#EAFAFF] shadow-lg rounded-lg px-4 py-1 mb-6 flex items-center space-x-4 border border-[#CBE2E4]">
           <img
-            src={profile} // Replace with actual avatar path
+            src={profile}
             alt="User Avatar"
-            className="w-12 h-12 rounded-full"
+            className="object-cover w-10 h-10 rounded-full"
           />
           <div className="mr-20">
-            <p className="text-gray-800 font-medium">Mauro M. Smith</p>
+            <p className="text-[#5F5F5F] font-medium text-xs">Mauro M. Smith</p>
             <p className="text-[#00969F] text-sm">Marketing Head</p>
           </div>
           <select className="ml-10 rounded-md p-2 text-gray-600 bg-[#EAFAFF]">
@@ -43,21 +41,21 @@ const Dashboard = () => {
           </select>
         </div>
 
-        <div className="mb-5 mt-5 flex items-center w-[50vw] sm:w-[50vw]">
-          <div className="bg-[#E4E4E4] h-[2px] w-full"></div>
-          <h1 className="bg-gray-100 px-5 py-1 border rounded-3xl text-xs w-[90vw] text-center">Choose your app</h1>
-          <div className="bg-[#E4E4E4] h-[2px] w-full"></div>
+        <div className="mb-5 mt-5 flex items-center justify-center w-[75vw] sm:w-[50vw]">
+          <div className="bg-[#E4E4E4] h-[2px] w-[20vw] sm:w-[12vw]"></div>
+          <h1 className="py-1 border rounded-3xl text-xs sm:w-[10vw] text-center text-slate-500">Choose your app</h1>
+          <div className="bg-[#E4E4E4] h-[2px] w-[20vw] sm:w-[12vw]"></div>
         </div>
 
         {/* Options */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-y-5 sm:gap-5 sm:pb-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-y-5 sm:gap-x-1 sm:gap-y-5 sm:pb-8">
           <OptionCard
             title="Sales"
             icon={salesIcon}
           />
           <OptionCard
             title="CRM"
-            icon={crmIcon}
+            icon={crmImage}
           />
           <OptionCard
             title="Settings"
@@ -69,17 +67,17 @@ const Dashboard = () => {
     </div>
   );
 };
-
+// F6F6F6 E0EBEB
 const OptionCard = ({ title, icon }) => {
   return (
-    <div className="bg-white rounded-lg px-3 sm:px-5 py-5 text-center shadow-lg transition-shadow border border-slate-300 mx-5 hover:scale-95 duration-300 ease-in-out flex flex-col">
-      <div className="text-4xl mb-2 item-center bg-gray-200 flex justify-center items-center rounded-full px-3 py-3 sm:px-5 sm:py-5 max-w-[50%] sm:max-w-[75%] mx-auto">
-        <img src={icon} alt="Icon" className="w-7 sm:w-10"/>
+    <div className="bg-white rounded-lg px-3 sm:px-5 py-5 text-center shadow-lg transition-shadow border border-[#E1E1E1] mx-5 flex flex-col">
+      <div className="text-4xl mb-2 item-center flex justify-center items-center px-3 py-3 sm:px-5 sm:py-5 max-w-[50%] sm:max-w-[100%] mx-auto rounded-full bg-gradient-to-b from-[#F6F6F6] to-[#E0EBEB]">
+        <img src={icon} alt="Icon" className="w-35 sm:w-10" style={{shapeOutside: 'circle()'}}/>
       </div>
-      <h2 className="text-md sm:text-lg">{title}</h2>
+      <h2 className="text-sm sm:text-md text-[#5F5F5F]">{title}</h2>
       <div className="mt-3 sm:mt-4">
-        <button className="px-6 py-2 bg-[#EAFAFF] text-[#00969F] hover:text-[#EAFAFF] rounded-md hover:bg-[#00969F] duration-300 border border-[#00969F] text-md sm:text-lg">
-          Open
+        <button className="px-5 py-1 bg-[#EAFAFF] text-[#067C83] hover:text-[#EAFAFF] rounded-md hover:bg-[#00969F] duration-300 border border-[#00969F] text-xs">
+          OPEN
         </button>
       </div>
     </div>
@@ -87,7 +85,3 @@ const OptionCard = ({ title, icon }) => {
 };
 
 export default Dashboard;
-
-{/* <button className="mt-3 sm:mt-4  px-6 py-2 bg-[#EAFAFF] text-[#00969F] hover:text-[#EAFAFF] rounded-md hover:bg-[#00969F] border border-[#00969F] text-md sm:text-lg">
-        Open
-      </button> */}
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -8,59 +8,59 @@ import bgImage from "../assets/pvsBg.jpg";
 
 const LoginForm = () => {
   return (
-    <div className="flex items-center justify-center h-[100vh] bg-gradient-to-r from-[#35C0C8] overflow-y-auto"
+    <div className="flex items-center justify-center min-h-[100vh] bg-gradient-to-r from-[#35C0C8] overflow-y-auto"
     style={{
       backgroundImage: `url(${bgImage})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
     }}>
       
-      <div className="w-full max-w-md px-16 py-14 bg-white rounded-lg shadow-md">
-        <div className="flex items-center justify-center h-full hover:scale-105 duration-300 ease-in-out">
+      <div className="w-[90vw] md:w-[50vw] px-16 py-14 bg-white rounded-lg shadow-md">
+        <div className="flex items-center justify-center h-full">
           <img src={pvsIcon} alt="PVSIcon" className="h-10" />
         </div>
-        <h2 className="text-lg font-bold text-center text-gray-800 mt-7 hover:scale-95 duration-300 ease-in-out">Login Your Account</h2>
-        <h3 className="text-sm text-center text-gray-500 pb-6 hover:scale-95 duration-300 ease-in-out">Lorem Ipsum is simply dummy text of the printing.</h3>
+        <h2 className="text-md text-center text-[#424341] mt-7">Login Your Account</h2>
+        <h3 className="text-sm text-center text-[#848484] pb-6">Lorem Ipsum is simply dummy text of the printing.</h3>
 
         <form className="space-y-4">
 
           {/* Email Field */}
-            <div className="relative flex gap-x-3 shadow-lg p-4 rounded-lg bg-white border border-3xl hover:scale-95 duration-300 ease-in-out">
+            <div className="relative flex gap-x-3 shadow-lg p-4 rounded-lg bg-white border border-3xl">
               {/* Icon Container */}
               <div className="flex items-center justify-center px-2 rounded-lg bg-gradient-to-t from-[#CAF1F2] to-[#F1FEFF]">
                 <img src={emailIcon} alt="PVSIcon" className="w-6" />
               </div>
               {/* Input Container */}
               <div className="flex-1">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="email" className="block text-sm font-medium text-[#51534F]">
                   Email ID
                 </label>
                 <input
                   id="email"
                   type="email"
                   required
-                  className="w-full px-4 py-1 border rounded-lg focus:ring focus:ring-blue-300 focus:outline-none text-sm"
+                  className="w-full py-1 rounded-lg focus:ring focus:ring-[#F1FEFF] outline-none text-sm text-[#9DA0A7]"
                   placeholder="Enter Email ID"
                 />
               </div>
             </div>
 
             {/* Password Field */}
-            <div className="relative flex gap-x-3 shadow-lg p-4 rounded-lg bg-white border border-3xl hover:scale-95 duration-300 ease-in-out">
+            <div className="relative flex gap-x-3 shadow-lg p-4 rounded-lg bg-white border border-3xl">
               {/* Icon Container */}
               <div className="flex items-center justify-center px-2 rounded-lg bg-gradient-to-t from-[#CAF1F2] to-[#F1FEFF]">
                 <img src={lockIcon} alt="PVSIcon" className="w-6" />
               </div>
               {/* Input Container */}
               <div className="flex-1">
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="password" className="block text-sm font-medium text-[#51534f]">
                   Password
                 </label>
                 <input
                   id="password"
                   type="password"
                   required
-                  className="w-full px-4 py-1 border rounded-lg focus:ring focus:ring-blue-300 focus:outline-none text-sm"
+                  className="w-full py-1 rounded-lg focus:ring focus:ring-[#F1FEFF] outline-none text-sm text-[#9DA0A7]"
                   placeholder="Enter Password"
                 />
               </div>
@@ -72,7 +72,7 @@ const LoginForm = () => {
             <Link to="/dashboard">
               <button
                 type="submit"
-                className="w-full p-3 text-xs md:text-md  bg-gradient-to-b from-teal-400 to-teal-500 text-white font-bold rounded-lg hover:bg-blue-700 focus:ring focus:ring-blue-300 focus:outline-none hover:scale-95 duration-300 ease-in-out shadow-lg"
+                className="w-full p-3 text-xs md:text-md  bg-gradient-to-b from-teal-400 to-teal-500 text-white font-bold rounded-lg hover:bg-blue-700 focus:ring focus:ring-blue-300 focus:outline-none shadow-lg"
               >
                 LOGIN
               </button>
